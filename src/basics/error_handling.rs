e0b89ecef8dbb8c19782d9fc6572c6f93a55e67d
Aritra <aritra119.26@gmail.com>
2020-08-13 19:22:07 +0530

    Added error handling and test samples

--- src/basics/error_handling.rs
+++ src/basics/error_handling.rs
@@ -1,1 +1,18 @@
-// todo: add example
+use std::fs::File;
+
+// unrecoverable errors
+#[allow(dead_code)]
+fn panic_error_example() {
+    let v = vec![1, 2, 3];
+    v[99];
+}
+
+// recoverable errors
+#[allow(dead_code)]
+fn rec_error_example() {
+    let f = File::open("hello.txt");
+    let _: File = match f {
+        Ok(res) => res,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+}
--- src/tests/vars_tests.rs
+++ src/tests/vars_tests.rs
@@ -9,3 +9,14 @@ fn is_palindrome_false_test() {
 fn is_palindrome_true_test() {
     assert_eq!(true, is_palindrome("aba".to_string()));
 }
+
+#[test]
+fn add_test() {
+    assert_eq!(5, add(2, 3));
+}
+
+#[test]
+fn get_type_test() {
+    assert_eq!(String::from("even"), get_type(6).1);
+    assert_eq!(String::from("odd"), get_type(5).1);
+}
